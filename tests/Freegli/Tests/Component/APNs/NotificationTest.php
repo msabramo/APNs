5497b6e6895bde40bc360f4a61d06ad81cb5c953
Marc Abramowitz <marc@marc-abramowitz.com>
2012-06-20 21:21:34 -0700

    Add explicit timezone of +01:00 (Europe/Paris) to DateTime so the test doesn't fail when run in other timezones, including Travis CI.

--- tests/Freegli/Tests/Component/APNs/NotificationTest.php
+++ tests/Freegli/Tests/Component/APNs/NotificationTest.php
@@ -17,7 +17,7 @@ class NotificationTest extends \PHPUnit_Framework_TestCase
     {
         $notification = new Notification();
         $notification->setIdentifier(2);
-        $notification->setExpiry(new \DateTime('2010-01-13 00:00:00'));
+        $notification->setExpiry(new \DateTime('2010-01-13T00:00:00+01:00'));
         $notification->setDeviceToken('4333526ff2e8b19730cab08c7a14f8b59e80aed473e06d6a2faa95bd82c3556e');
         $notification->setPayload(array(
             'aps' => array(
